2e3c996e98d12bb61bcc5ef546a8046c4baad77d
nathan <nathan.consolo17@gmail.com>
2024-11-13 18:56:51 -0300

    Melhorando interacao do menu para dispositivos moveis

--- carbon-track/src/components/Cabecalho/Cabecalho.tsx
+++ carbon-track/src/components/Cabecalho/Cabecalho.tsx
@@ -1,8 +1,9 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Menu from "@/icons/menu-100x100.png";
+import "@/app/styles/inicial.css"
 
 export default function Cabecalho() {
   const router = useRouter();
@@ -10,43 +11,68 @@ export default function Cabecalho() {
 
   const navegar = (destino: string) => {
     router.push(destino);
-    setMenuAberto(false); 
+    setMenuAberto(false);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement | null;
+      if (menuAberto && target && !target.closest(".menu-container")) {
+        setMenuAberto(false);
+      }
+    };
+
+    if (menuAberto) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuAberto]);
 
   return (
     <>
-
-      <header className=" block hidden xl:flex bg-primary text-white w-full p-4">
+      <header className="block hidden xl:flex bg-primary text-white w-full p-4">
         <div className="container mx-auto flex items-center justify-between">
           <nav className="flex space-x-6">
             <button
-              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-secondary group-hover:from-green-400 group-hover:to-secondary hover:text-black dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200"
+              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-secondary group-hover:from-green-400 group-hover:to-secondary hover:text-black dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
               onClick={() => navegar("/")}
             >
-              Início
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Início
+              </span>
             </button>
             <button
-              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-secondary group-hover:from-green-400 group-hover:to-secondary hover:text-black dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200"
+              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-secondary group-hover:from-green-400 group-hover:to-secondary hover:text-black dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
               onClick={() => navegar("/sobre")}
             >
-              Sobre
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Sobre nós
+              </span>
             </button>
             <button
-              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-secondary group-hover:from-green-400 group-hover:to-secondary hover:text-black dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200"
+              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-secondary group-hover:from-green-400 group-hover:to-secondary hover:text-black dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
               onClick={() => navegar("/servicos")}
             >
-              Calcular emissão
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Calcular emissão
+              </span>
             </button>
             <button
-              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-secondary group-hover:from-green-400 group-hover:to-secondary hover:text-black dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200"
+              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-secondary group-hover:from-green-400 group-hover:to-secondary hover:text-black dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
               onClick={() => navegar("/contato")}
             >
-              Contato
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Integrantes
+              </span>
             </button>
           </nav>
         </div>
       </header>
-
 
       <div className="xl:hidden fixed top-4 right-4 z-50">
         <div className="rounded-full bg-primary p-1">
@@ -54,14 +80,14 @@ export default function Cabecalho() {
             alt="Menu"
             src={Menu}
             width={40}
-            onClick={() => setMenuAberto(!menuAberto)} 
+            onClick={() => setMenuAberto(!menuAberto)}
           />
         </div>
       </div>
 
       {menuAberto && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-end z-40">
-          <div className="w-[36vw] h-[80vh] bg-primary text-white p-4 space-y-4 flex flex-col justify-center">
+          <div className="MenuContainer">
             <span
               onClick={() => navegar("/")}
               className="block hover:underline cursor-pointer"
@@ -84,7 +110,7 @@ export default function Cabecalho() {
               onClick={() => navegar("/contato")}
               className="block hover:underline cursor-pointer"
             >
-              Contato
+              Integrantes
             </span>
           </div>
         </div>
