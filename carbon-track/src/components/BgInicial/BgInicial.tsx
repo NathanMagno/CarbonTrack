5d2300e7667aef435b33023397993e0a22fb5966
nathan <nathan.consolo17@gmail.com>
2024-11-13 23:37:59 -0300

    Base das paginas criada

---
+++ carbon-track/src/components/BgInicial/BgInicial.tsx
@@ -0,0 +1,13 @@
+
+
+export default function BgInicial() {
+  return (
+    <main className="BgInicial">
+
+        <div className="ContainerFuncionaliodade"></div>
+        <div className="ContainerFuncionaliodade"></div>
+        <div className="ContainerFuncionaliodade"></div>
+
+    </main>
+  )
+}
--- carbon-track/src/components/Cabecalho/Cabecalho.tsx
+++ carbon-track/src/components/Cabecalho/Cabecalho.tsx
@@ -56,7 +56,7 @@ export default function Cabecalho() {
             </button>
             <button
               className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-secondary group-hover:from-green-400 group-hover:to-secondary hover:text-black dark:text-white focus:outline-none"
-              onClick={() => navegar("/servicos")}
+              onClick={() => navegar("/calcular")}
             >
               <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 Calcular emissão
@@ -64,7 +64,7 @@ export default function Cabecalho() {
             </button>
             <button
               className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-secondary group-hover:from-green-400 group-hover:to-secondary hover:text-black dark:text-white focus:outline-none"
-              onClick={() => navegar("/contato")}
+              onClick={() => navegar("/integrantes")}
             >
               <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 Integrantes
